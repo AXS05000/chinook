2084dc333023dc9e7961d9eb3ba553cc9e168a1b
AXS05000 <axssxs00@gmail.com>
2024-05-24 23:24:48 -0300

    V - 23:25 - 24-05-2024

--- ia/urls.py
+++ ia/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import chat_view, generate_excel_report, ExcelImportView
 
 urlpatterns = [
-    path("chat/", chat_view, name="chat"),
+    path("chinook/", chat_view, name="chat"),
     path("generate_report/", generate_excel_report, name="generate_report"),
     path("importar_nps/", ExcelImportView.as_view(), name="importar_nps"),
 ]
