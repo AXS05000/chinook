28ea979a87c62b25d4059694f78deabd6710ef71
AXS05000 <axssxs00@gmail.com>
2024-07-28 01:56:42 -0300

    V - 01:56 - 28-07-2024

--- ia/urls.py
+++ ia/urls.py
@@ -5,13 +5,11 @@ from .views import (
     hr_assistant_view,
     import_crm_fui,
     import_resposta,
-    ExcelImportView,
 )
 
 urlpatterns = [
     path("chinook/", chat_view, name="chat"),
     path("generate_report/", generate_excel_report, name="generate_report"),
-    path("importar_nps/", ExcelImportView.as_view(), name="importar_nps"),
     path("hr/", hr_assistant_view, name="hr_assistant"),
     path("import_crm_fui/", import_crm_fui, name="import_crm_fui"),
     path("import_respostas_nps/", import_resposta, name="import_respostas_nps"),
