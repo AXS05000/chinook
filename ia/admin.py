c331544bd3357ecdc3af73b22709cbf47b57f3b2
Alex Sobreira <alexs.oliveira@sebsa.com.br>
2024-08-11 02:28:43 -0300

    V - 11-08-2024 - 02:28

--- ia/admin.py
+++ ia/admin.py
@@ -13,6 +13,7 @@ from .models import (
     Vendas_SLM_2025,
     Base_de_Conhecimento,
     Planificador_2024,
+    Resumo_Respostas_NPS,
 )
 # Register your models here.
 admin.site.register(Informacao)
@@ -28,3 +29,4 @@ admin.site.register(Vendas_SLM_2024)
 admin.site.register(Vendas_SLM_2025)
 admin.site.register(Base_de_Conhecimento)
 admin.site.register(Planificador_2024)
+admin.site.register(Resumo_Respostas_NPS)
