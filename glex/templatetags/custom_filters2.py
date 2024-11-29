ba60613e2b04a91f3b1166a5d2021ecdbd238b53
Alex Sobreira <alexs.oliveira@sebsa.com.br>
2024-11-29 11:24:12 -0300

    V - 29-11-2024 - 11:24

--- glex/templatetags/custom_filters2.py
+++ glex/templatetags/custom_filters2.py
@@ -4,8 +4,13 @@ from django import template
 register = template.Library()
 
 @register.filter
-def basename(value):
-    """Retorna apenas o nome do arquivo, ou uma string vazia se o valor for None."""
+def basename(value, max_length=None):
+    """
+    Retorna o nome base do arquivo e opcionalmente trunca o nome se max_length for fornecido.
+    """
     if value:
-        return os.path.basename(value)
+        base_name = os.path.basename(value)
+        if max_length and len(base_name) > int(max_length):
+            return base_name[:int(max_length)] + "..."
+        return base_name
     return ""
