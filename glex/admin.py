e17ce0ab9b5294215d5b3a3cde39f6656d1d8abe
Alex Sobreira <alexs.oliveira@sebsa.com.br>
2024-12-10 13:37:05 -0300

    V - 10-12-2024 - 13:36

--- glex/admin.py
+++ glex/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
-
 from .models import (
     Base_de_Conhecimento_Geral,
-
+    GlexGestaoDeParceria,
+    GlexGente,
+    GlexAdministrativo,
+    GlexTecnologia,
+    GlexMarketing,
+    GlexAcademico,
+    GlexGestaoEscolar,
+    GlexOperacaoAcademica,
+    GlexImplantacao,
+    GlexComercial,
+    GlexResultado,
+    Dominio1,
+    Dominio2,
+    Dominio3,
+    Dominio4,
 )
 
 
 admin.site.register(Base_de_Conhecimento_Geral)
+admin.site.register(GlexGestaoDeParceria)
